08e41cec1c70c5d0246ee7c8fa551078809e20f3
Rofernan1205 <rodrigo.1997.fm@gmail.com>
2026-01-28 14:36:03 -0500

    bender 22 tablas

--- app/models/roles.py
+++ app/models/roles.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
 from typing import List, TYPE_CHECKING
--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from app.models import Base
 from typing import List, TYPE_CHECKING, Optional
-from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy import String, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 
@@ -9,7 +10,7 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(80), nullable=False)
     last_name: Mapped[str] = mapped_column(String(80), nullable=False)
     username: Mapped[str] = mapped_column(String(15), nullable=False, unique=True, index=True)
-    password: Mapped[str] = mapped_column(String(25), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
     email: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
     phone: Mapped[str] = mapped_column(String(15),nullable=False)
     address: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
@@ -17,9 +18,11 @@ class User(Base):
     branch_id :Mapped[int] = mapped_column(ForeignKey('branches.id'), nullable=False)
     role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'), nullable=False)
 
+    last_login :Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+
 
     def __repr__(self) -> str:
         return f"<Users {self.name}, {self.last_name}, {self.username} >"
 
-    #https://www.oechsle.pe/laptop-lenovo-loq-8gb-ram-intel-core-i5-12450hx-2925700/p?gad_campaignid=22485560843#
+
 
